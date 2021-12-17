53ab0ff3d2a77c3eb746ed888f78625b3118ea1a
jake <you@example.com>
2021-12-17 22:16:58 +0800

    new udpates for serverless

--- project/app.py
+++ project/app.py
@@ -1,4 +1,5 @@
 import pickle
+import traceback
 
 import numpy as np
 from flask import Flask, request
@@ -13,13 +14,17 @@ model = pickle.load(open(MODELPATH, 'rb'))
 @app.route("/predict", methods=["POST"])
 def predict():
     """{"input": [5.8, 2.8, 5.1, 2.4]}"""
-    content = request.json
-    sample = content["input"]
+    try:
+        content = request.json
+        sample = content["input"]
 
-    sample = np.array(sample).reshape(1, -1)
-    prediction = model.predict(sample).tolist()[0]
+        sample = np.array(sample).reshape(1, -1)
+        prediction = model.predict(sample).tolist()[0]
 
-    return {"prediction": prediction}
+        return {"prediction": prediction}
+    except Exception as e:
+        tb = traceback.format_exc()
+        return {"errorMessages": tb.replace("\n","")}
 
 
 if __name__ == "__main__":
